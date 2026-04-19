7612828e48300b57fa2ded82b7e9e5e4e01c8d99
nfinity <aallas@hkhk.edu.ee>
2026-04-19 17:44:22 +0300

    d

---
+++ php01.php
@@ -0,0 +1,13 @@
+<?php
+
+$nimi = "Aallas";
+$kuupaev = 19.04;
+
+echo $nimi . PHP_EOL . PHP_EOL . PHP_EOL;
+
+echo "Väljasta järgnev lause: “Tere Maailm!”" . PHP_EOL;
+
+echo "   /\\_/\\   " . PHP_EOL;
+echo "  ( o.o )  " . PHP_EOL;
+echo "   > ^ <   " . PHP_EOL;
+?>
